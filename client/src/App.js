f784e7df1a2184a660032b326562f80e81c45790
Ansu Jammeh <ansuj9@yahoo.com>
2023-02-21 18:55:37 +0000

    not yet completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,44 @@
-import { Route, Routes } from "react-router-dom";
-
-import About from "./pages/About";
+import React from "react";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Cards from "./pages/Cards";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Link } from "react-router-dom";
+import ContactUs from "./pages/ContactUs";
+import SignUp from "./pages/SignUp";
+import Navbar from "./components/navbar/NavBar";
+import Gameboard from "./pages/Gameboard";
+import Login from "./pages/Login";
 
-const App = () => ({
-	<Routes>
-		<Route path="/" element={<Home />} />
-		<Route path="/about/this/site" element={<About />} />
-	</Routes>
-);
+function App() {
+	return (
+		<Router>
+			<Navbar />
 
+
+			<Switch>
+				<Route path="/" components={Home} exact>
+					<Home />
+				</Route>
+
+				<Route path="/About" components={About} exact>
+					<About />
+				</Route>
+
+				<Route path="/ContactUs" components={ContactUs} exact>
+					<ContactUs />
+				</Route>
+
+				<Route path="/Login" components={Login} exact>
+					<Login />
+				</Route>
+
+				<Route path="/SignUp" components={SignUp} exact>
+					<SignUp />
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
-
-			{/* <Switch>
-        <Route path="/" component={Home} exact>
-          <Home />
-        </Route>
-
-        <Route path="/about" component={About} exact>
-          <About />
-        </Route>
-
-        <Route path="/contact" component={ContactUs} exact>
-          <ContactUs />
-        </Route>
-
-        <Route path="/signUp" component={Signup} exact>
-          <Signup />
-        </Route>
-
-        <Route path="/" component={Home} exact>
-          <Home />
-        </Route>
-
-        <Route path="/" component={Home} exact>
-          <Home />
-        </Route>
-      </Switch> */}
