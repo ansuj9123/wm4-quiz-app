877b3ba9b573a078944cf9601d90b0a9ec39127e
Sharifa Ismail <shareefahismail@gmail.com>
2023-03-06 11:51:12 +0000

    Added post for signup

--- server/api.js
+++ server/api.js
@@ -21,6 +21,26 @@ router.get("/quiz", (req, res) => {
 		});
 });
 
+router.post("/signup", async(req, res) => {
+	const {firstname, lastname, email, password, role} = req.body;
+	try{
+		const oldUser = await db.query(
+			"SELECT * FROM users WHERE username = $1", [username]
+		);
+				if (oldUser.rowCount > 0) {
+			return res.status(200).send("Username already exists");
+			}
+		
+			const result = await db.query(
+			"INSERT INTO users (first_name, last_name, email, password, role) VALUES ($1, $2, $3, $4, $5)", [firstname, lastname, email, password, role]);
+			res.status(200).send("User created successfully");
+			return;
+	 } catch(error) {
+		res.status(500).send("something went wrong");
+	 }
+});
+
+
 router.get("/questions", (req, res) => {
 	db.query("SELECT * FROM questions")
 		.then((result) => {
