692ce5d79032f26ee7e0f5b4d7b59c7c0c10c89b
ozibozkurt <ozipaki@gmail.com>
2023-03-10 10:29:17 +0000

    registration api

--- server/api.js
+++ server/api.js
@@ -1,10 +1,12 @@
-
 import { Router } from "express";
 
+const jwt = require("jsonwebtoken");
+const { check, validationResult } = require("express-validator");
 import logger from "./utils/logger";
-// import { handleSignUp } from "./controllers/SignUp";
 import db from "./db";
+import bcrypt from "bcrypt";
 
+const secret = "mysecret";
 const router = Router();
 
 router.get("/", (_, res) => {
@@ -12,25 +14,56 @@ router.get("/", (_, res) => {
 	res.json({ message: "Hello, world!" });
 });
 
-router.get("/quiz", (req, res) => {
-	db.query("SELECT * FROM quiz")
-		.then((result) => {
-			res.json(result.rows);
-		})
-		.catch((error) => {
-			res.status(500).json(error);
-		});
-});
+router.post(
+	"/register",
+	[
+		check("email", "Please include a valid email").isEmail(),
+		check(
+			"password",
+			"Please enter a password with 6 or more characters"
+		).isLength({
+			min: 6,
+		}),
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		const { first_name, last_name, email, password, role } = req.body;
+		try {
+			let user = await db.query("SELECT * FROM users WHERE email = $1", [
+				email,
+			]);
+			if (user.rows.length > 0) {
+				return res
+					.status(400)
+					.json({ errors: [{ msg: "User already exists" }] });
+			}
+			const salt = await bcrypt.genSalt(10);
+			const hashedPassword = await bcrypt.hash(password, salt);
+			const newUser = await db.query(
+				"INSERT INTO users (first_name,last_name,email,password,role) VALUES ($1,$2,$3,$4,$5) RETURNING *",
+				[first_name, last_name, email, hashedPassword, role]
+			);
+			const payload = {
+				user: {
+					id: newUser.rows[0].id,
+					role: newUser.rows[0].role, // add role to payload
+				},
+			};
+			jwt.sign(payload, secret, { expiresIn: 360000 }, (err, token) => {
+				if (err) {
+					throw err;
+				}
+				res.json({ token });
+			});
+		} catch (err) {
+			logger.error(err.message);
+			res.status(500).send("Server error");
+		}
+	}
+);
 
-router.get("/questions", (req, res) => {
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	db.query("SELECT * FROM questions")
-		.then((result) => {
-			res.json(result.rows);
-		})
-		.catch((error) => {
-			res.status(500).json(error);
-		});
-});
 
 export default router;
