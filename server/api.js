d59960f7973c8e3ccf821f54b8690a01ac9024ac
ozibozkurt <ozipaki@gmail.com>
2023-03-10 10:30:36 +0000

    login api

--- server/api.js
+++ server/api.js
@@ -65,5 +65,51 @@ router.post(
 	}
 );
 
+router.post(
+	"/login",
+	[
+		check("email", "Please include a valid email").isEmail(),
+		check("password", "Password is required").exists(),
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		const { email, password } = req.body;
+		try {
+			let user = await db.query("SELECT * FROM users WHERE email = $1", [
+				email,
+			]);
+			if (user.rows.length === 0) {
+				return res
+					.status(400)
+					.json({ errors: [{ msg: "Invalid credentials" }] });
+			}
+			const isMatch = await bcrypt.compare(password, user.rows[0].password);
+			if (!isMatch) {
+				return res
+					.status(400)
+					.json({ errors: [{ msg: "Invalid credentials" }] });
+			}
+			const payload = {
+				user: {
+					id: user.rows[0].id,
+					role: user.rows[0].role, // add role to payload
+				},
+			};
+			jwt.sign(payload, secret, { expiresIn: 360000 }, (err, token) => {
+				if (err) {
+					throw err;
+				}
+				res.json({ token });
+			});
+		} catch (err) {
+			logger.error(err.message);
+			res.status(500).send("Server error");
+		}
+	}
+);
+
 
 export default router;
