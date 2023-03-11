78bf0798c3b431e0dc7de1e4489713a17f68a3b8
Petar Nikolov <100172752+Nikolov87@users.noreply.github.com>
2023-03-11 20:14:26 +0000

    css-students

--- client/src/pages/Student.js
+++ client/src/pages/Student.js
@@ -10,11 +10,11 @@ export default function BasicButtons() {
 	return (
 		<div className="stud" spacing={2} direction="row">
 			<span className="theText">
-				<p className="jsTag"><strong>Java Script</strong><br />Description: This will test your knowledge of JavaScript!</p>
+				<p className="jsTag"><strong>&lt; Java Script &gt; <br /> &lt; NodeJS &gt;<br />&lt; SQL &gt;</strong><br />Description: This will test your knowledge!</p>
 				<ul className="links">
 					<li className="basic-item">
 						<Link to="/StartQuiz" className="basic">
-            <strong>Difficulty: </strong>Basic
+            <strong>Category: </strong>JacaScript
             <KeyboardArrowDownIcon style={{ marginBottom: "-11px", marginLeft: "8rem", fontSize: "2rem" }} />
 						</Link>
 					</li>
@@ -22,16 +22,16 @@ export default function BasicButtons() {
 				<ul className="links">
 					<li className="medium-item">
 						<Link to="/StartQuiz" className="medium" >
-            <strong>Difficulty: </strong>Medium
-            <KeyboardArrowDownIcon style={{ marginBottom: "-11px", marginLeft: "107px", fontSize: "2rem" }} />
+            <strong>Category: </strong>Node JS
+            <KeyboardArrowDownIcon style={{ marginBottom: "-11px", marginLeft: "8rem", fontSize: "2rem" }} />
 						</Link>
 					</li>
 				</ul>
 				<ul className="links">
 					<li className="challenging-item">
 						<Link to="/StartQuiz" className="challenging">
-            <strong>Difficulty: </strong>Challenging
-            <KeyboardArrowDownIcon style={{ marginBottom: "-11px", marginLeft: "5rem", fontSize: "2rem" }} />
+            <strong>Category: </strong>SQL
+            <KeyboardArrowDownIcon style={{ marginBottom: "-11px", marginLeft: "9rem", fontSize: "2rem" }} />
 						</Link>
 					</li>
 				</ul>
