347ab26012a4c05a7b5fd82676146918147f243e
Sharifa1 <105314490+Sharifa1@users.noreply.github.com>
2023-03-15 21:46:43 +0000

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,6 @@
 import Cards from "./Cards";
 import React from "react";
-import Sidebar from "./Sidebar";
+// import Sidebar from "./Sidebar";
 import "./Home.css";
 import Layout from "./Layout";
 import Footer from "./Footer";
@@ -16,7 +16,7 @@ function Home() {
 				Welcome to <span style={{ color: "red" }}>CodeYourFuture</span> Quiz App
 			</h4>
 			<Cards />
-			<Sidebar />
+// 			<Sidebar />
 			<Layout />
 			<Footer />
 		</div>
